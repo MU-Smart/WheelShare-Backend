6b9f268025ac0ed466461a40a1318efec8ac138e
Thomas Nguyen <nguyenk7@miamioh.com>
2022-10-28 23:28:38 -0400

    Weight Map Final Commit

--- src/main/java/com/example/springboot/Services/MapServiceBuilderImpl.java
+++ src/main/java/com/example/springboot/Services/MapServiceBuilderImpl.java
@@ -37,27 +37,23 @@ public class MapServiceBuilderImpl implements MapServiceBuilder {
 			String absoluteFilePath = new File("").getAbsolutePath();
 			log.info(absoluteFilePath);
 
+			// * Read the file in
 			Object jsonFileObject = parser.parse(new FileReader(absoluteFilePath + "/src/main/resources/mapData.json"));
-			// Object jsonFileObject = parser.parse(new FileReader(absoluteFilePath + "/routing/mapData.json"));
+			// Object jsonFileObject = parser.parse(new FileReader(absoluteFilePath +
+			// "/routing/mapData.json"));
 			JSONObject jsonObject = (JSONObject) jsonFileObject;
 
+			// * Clean up all of the hashmaps to put new data in
+			// ?: Is there any way we can pull new data in without having to clear the
+			// hashmaps?
 			nodeMap.clear();
 			edgeMap.clear();
 			weightMap.clear();
 
-			/*
-			 * Handles node list processing from file
-			 */
 			JSONObject osmJsonObject = (JSONObject) jsonObject.get("osm");
 			JSONArray jsonNodeList = (JSONArray) osmJsonObject.get("node");
 
-			log.info("Test Info");
-			log.info(osmJsonObject.get("@version").getClass().toString());
-			log.info(osmJsonObject.get("@generator").getClass().toString());
-			log.info(osmJsonObject.get("note").getClass().toString());
-			log.info(osmJsonObject.get("node").getClass().toString());
-
-			// ------------------------------------ Node Map ------------------------------------//
+			// -----------------Node Map-----------------
 			for (int i = 0; i < jsonNodeList.size(); i++) {
 				JSONObject currNode = (JSONObject) jsonNodeList.get(i);
 
@@ -78,45 +74,91 @@ public class MapServiceBuilderImpl implements MapServiceBuilder {
 				nodeMap.put(nodeId, mapNode);
 			}
 
-			// ------------------------------------ Way Map ------------------------------------//
+			// -----------------Edge and Weight Map-----------------
 			JSONArray wayList = (JSONArray) osmJsonObject.get("way");
-			log.info(String.format("Class Name : %s", wayList.getClass().toString()));
+			int unlabelledWayCount = 0; // a counter to count how many ways does not have a valid incline
+
 			for (int i = 0; i < wayList.size(); i++) {
 				JSONObject currWay = (JSONObject) wayList.get(i);
 				JSONArray currWayNodeList = (JSONArray) currWay.get("nd");
+				double weight = 1;
+
+				/**
+				 * * Check for the incline inside each way.
+				 * * If there is, set the weight var to that value
+				 * * If not, log out an error and increment the var unlabelledWayCount
+				 */
+				if (currWay.get("tag") == null) {
+					unlabelledWayCount++;
+					log.warn(String.format("Tag not available for this way: Id - %s", currWay.get("@id")));
+				} else {
+					// * Tag can be either null or an object or an array
+					String tagType = currWay.get("tag").getClass().toString();
+
+					if (tagType.equals("class org.json.simple.JSONArray")) {
+						JSONArray tagList = (JSONArray) currWay.get("tag");
+						for (int index = 0; index < tagList.size(); index++) {
+							JSONObject currTag = (JSONObject) tagList.get(index);
+
+							if (currTag.get("@k").toString().equals("incline")) {
+								try {
+									weight = Double.parseDouble(currTag.get("@v").toString());
+								} catch (NumberFormatException e) {
+									unlabelledWayCount++;
+									log.info(String.format("Incline is not a number at way : %s", currWay.get("@id")));
+								}
+							}
+						}
+					}
+
+					if (tagType.equals("class org.json.simple.JSONObject")) {
+						JSONObject tagOject = (JSONObject) currWay.get("tag");
+						if (tagOject.get("@k").toString().equals("incline")) {
+							try {
+								weight = Double.parseDouble(tagOject.get("@v").toString());
+							} catch (NumberFormatException e) {
+								unlabelledWayCount++;
+								log.info(String.format("Incline is not a number at way : %s", currWay.get("@id")));
+							}
+						}
+					}
+				}
 
 				for (int startIndex = 0; startIndex < currWayNodeList.size(); startIndex++) {
+					// -----------------Edge Map Data Processing-----------------
 					JSONObject startNodeObj = (JSONObject) currWayNodeList.get(startIndex);
 					long startNode = Long.parseLong(startNodeObj.get("@ref").toString());
 
 					for (int endIndex = startIndex + 1; endIndex < currWayNodeList.size(); endIndex++) {
 						JSONObject endNodeObj = (JSONObject) currWayNodeList.get(endIndex);
 						long endNode = Long.parseLong(endNodeObj.get("@ref").toString());
-						
+
 						List<Long> startNodeList = edgeMap.get(startNode);
 						List<Long> endNodeList = edgeMap.get(endNode);
 
-						if (startNodeList == null)	{
+						if (startNodeList == null) {
 							startNodeList = new ArrayList<Long>();
 							startNodeList.add(endNode);
 							edgeMap.put(startNode, startNodeList);
-						}	else	{
+						} else {
 							startNodeList.add(endNode);
 						}
 
-						if (endNodeList == null)	{
+						if (endNodeList == null) {
 							endNodeList = new ArrayList<Long>();
 							endNodeList.add(startNode);
 							edgeMap.put(endNode, endNodeList);
-						}	else	{
+						} else {
 							endNodeList.add(startNode);
 						}
 
-						// Pair<Long, Long> startEndNodePair = new Pair<Long, Long>(startNode, endNode);
-						// Pair<Long, Long> endStartNodePair = new Pair<Long, Long>(endNode, startNode);
+						// -----------------Weight Map Data Processing-----------------
 
+						Pair<Long, Long> startEndNodePair = new Pair<Long, Long>(startNode, endNode);
+						Pair<Long, Long> endStartNodePair = new Pair<Long, Long>(endNode, startNode);
 
-
+						weightMap.put(startEndNodePair, weight);
+						weightMap.put(endStartNodePair, weight);
 					}
 				}
 			}
@@ -124,6 +166,8 @@ public class MapServiceBuilderImpl implements MapServiceBuilder {
 			log.info("Succesfully constructed the map");
 			log.info(String.format("Node Map Size = %d", nodeMap.size()));
 			log.info(String.format("Edge Map Size = %d", edgeMap.size()));
+			log.info(String.format("Weight Map Size = %d", weightMap.size()));
+			log.info(String.format("Number of ways that missed valid incline value = %d", unlabelledWayCount));
 		} catch (Exception e) {
 			log.error("Error in buildMap function", e);
 			e.printStackTrace();
@@ -136,5 +180,9 @@ public class MapServiceBuilderImpl implements MapServiceBuilder {
 
 	public Map<Long, List<Long>> getEdgeMap() {
 		return edgeMap;
+	}
+
+	public Map<Pair<Long, Long>, Double> getWeightMap() {
+		return weightMap;
 	}
 }
